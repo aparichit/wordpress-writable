0da35f25d0d083a3b9c21fd51abc9bf6fe024442
3sidedcoin <tejas@3sided.co.in>
2013-07-17 17:42:22 +0530

    another commit

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ if [ "$(ls -A $DIR)" ]; then
 else
 	"(cd $DIR;wget http://wordpress.org/latest.tar.gz;tar xfz $DIR/latest.tar.gz;mv $DIR/wordpress/* $DIR/;rmdir $DIR/wordpress/;rm -f $DIR/latest.tar.gz)"
 fi
-"mv wp-config.php $DIR/wp-config.php"
+"mv /var/www/wp-config.php $DIR/wp-config.php"
 # rest of the logic
